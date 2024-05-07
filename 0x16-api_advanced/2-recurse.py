3116bb53bbbbd0a692f02c9c59b85650430991a2
Olaitan34 <fatkayus@gmail.com>
2024-05-07 16:56:49 +0100

    kk

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import requests
-
-"""program that returs a list containing 
-the titles of all hot articles for a given subreddit
 """
+Recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit,
+the function should return None.
+"""
+
+import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
@@ -13,23 +16,22 @@ def recurse(subreddit, hot_list=[], after=""):
 
     - If not a valid subreddit, return None.
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    req = requests.get(url, 
-                            headers="User-Aget":"Custom"
-                            params={"after":"after"},
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
     )
 
     if req.status_code == 200:
-        for GetData in req.json().get("data").get("children"):
-            dat = GetData.get("data")
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
             title = dat.get("title")
             hot_list.append(title)
         after = req.json().get("data").get("after")
-        
+
         if after is None:
             return hot_list
         else:
             return recurse(subreddit, hot_list, after)
     else:
         return None
-    
