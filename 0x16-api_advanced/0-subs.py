f67415c2d8335a0b99658d42f8da5b5650778965
Olaitan34 <fatkayus@gmail.com>
2024-05-07 15:33:12 +0100

    kk

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,20 @@
 """
 This program work with the Reddit API
 """
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """number_of_subscribers"""
-   url = "https://reddit.com/r/{}/about.json".format(subreddit)
-   req = requests.get(url)
-   
-   if req.status_code == 200:
+    url = "https://reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(url)
+
+    if req.status_code == 200:
         data = req.json()
         cribers = data["data"]["subscribers"]
         return cribers
-   elif req.status_code == 302:
-       return 0
-   else:
-       return 0
+    elif req.status_code == 302:
+        return 0
+    else:
+        return 0
